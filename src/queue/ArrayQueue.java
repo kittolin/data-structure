5f4e6a29bd8f869fdf1d2d0122ed0d7f1df093bf
Kitto <kittolin10@163.com>
2020-04-12 18:52:05 +0800

    queue based dynamic array

---
+++ src/queue/ArrayQueue.java
@@ -0,0 +1,74 @@
+package queue;
+
+import array.DynamicArray;
+
+import java.util.NoSuchElementException;
+
+public class ArrayQueue<E> implements Queue<E> {
+
+    private DynamicArray<E> array;
+
+    public ArrayQueue(int capacity) {
+        array = new DynamicArray<>(capacity);
+    }
+
+    public ArrayQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return array.getCapacity();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Queue: front [");
+        for(int i = 0; i < array.getSize(); i++) {
+            res.append(array.get(i));
+            if(i != array.getSize() - 1) {
+                res.append(", ");
+            }
+        }
+        res.append("] tail");
+        return res.toString();
+    }
+
+    /**
+     * Amortized time complexity: O(1)
+     */
+    @Override
+    public void enqueue(E e) {
+        array.addLast(e);
+    }
+
+    /**
+     * Time complexity: O(n), can use loop queue to optimize to O(1).
+     */
+    @Override
+    public E dequeue() {
+        if(array.isEmpty()) {
+            throw new NoSuchElementException("Cannot dequeue from an empty queue.");
+        }
+        return array.removeFirst();
+    }
+
+    @Override
+    public E getFront() {
+        if(array.isEmpty()) {
+            throw new NoSuchElementException("Queue is empty.");
+        }
+        return array.getFirst();
+    }
+
+}
